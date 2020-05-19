8c49ce713db11063660f698da845c5c85f89a8c7
zon <zon_click@mail.ru>
2020-05-19 21:49:02 +0300

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by ADMIN on 19.05.2020.
+//  Copyright © 2020 Arkadij Ryabov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
